f5a2f524ff10b68985cc39fd421c796876f9ee36
amirahmed002 <aaashaikh86@gmail.com>
2023-08-24 10:40:09 +0530

    updated version

---
+++ Database/SriptForDummyData.sql
@@ -0,0 +1,112 @@
+-- Assuming you're using MySQL or a similar database
+USE DOCO;
+
+-- Disable foreign key checks temporarily
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- Truncate each table
+
+TRUNCATE TABLE DayAvailables;
+TRUNCATE TABLE Appointments;
+TRUNCATE TABLE Patients;
+TRUNCATE TABLE Transactions;
+TRUNCATE TABLE Schedules;
+TRUNCATE TABLE Doctors;
+TRUNCATE TABLE Users;
+TRUNCATE TABLE Specialities;
+TRUNCATE TABLE Roles;
+TRUNCATE TABLE SubscriptionPlans;
+TRUNCATE TABLE Questions;
+TRUNCATE TABLE Days;
+TRUNCATE TABLE Locations;
+
+-- Enable foreign key checks again
+SET FOREIGN_KEY_CHECKS = 1;
+USE DOCO;
+INSERT INTO Roles (id_, role_type_) VALUES
+(1, 'ADM'),
+(2, 'DOC'),
+(3, 'PAT');
+
+-- Specialities
+INSERT INTO Specialities (id_, specialities_Name_) VALUES
+(1, 'Dermatology'),
+(2, 'Cardiology'),
+
+(3, 'Neurology'),
+(4, 'Orthopedics'),
+(5, 'Gynaecology'),
+
+(6, 'Oncologist'),
+(7, 'Gastroenterologist'),
+(8, 'Pediatrics'),
+(9, 'Radiology'),
+(10, 'Endocrinologist');
+
+-- Locations
+INSERT INTO Locations (id_, location_Name_) VALUES
+(1, 'Pimpri-Chinchwad'),
+(2, 'Viman Nagar'),
+(3, 'Baner'),
+(4, 'koregaon Park'),
+(5, 'Hadapsar'),
+(6, 'kothrud'),
+(7, 'kharadi'),
+(8, 'Hinjewadi'),
+(9, 'Shivaji Nagar'),
+(10, 'Deccan'),
+(11, 'kondwa');
+
+-- SubscriptionPlans
+INSERT INTO SubscriptionPlans (id_, plan_name_, descripOfSub_, monthly_price_, status_) VALUES
+(1, 'Basic', 'Basic Plan', 50, 1),
+(2, 'Standard', 'Standard Plan', 100, 1),
+(3, 'Premium', 'Premium Plan', 200, 1);
+
+-- Questions
+INSERT INTO Questions (id_, questions_) VALUES
+(1, 'What is your nick name ?'),
+(2, 'What is your native place ?'),
+(3, 'What is your fvt movie?'),
+(4, 'What is your fvt actor ?'),
+(5, 'What is your fvt color ?');
+
+-- Days
+INSERT INTO Days (id_, day_) VALUES
+(1, 'Monday'),
+(2, 'Tuesday'),
+(3, 'Wednesday'),
+(4, 'Thursday'),
+(5, 'Friday');
+
+-- Users
+INSERT INTO Users (id_, role_id_, email_,password_, question_id_, answer_, status_) VALUES
+(1, 1, 'admin@example.com', 'admin_password', 5, 'Blue', 0),
+(2, 2, 'D@example.com', '123', 5, 'Blue', 1),
+(3, 3, 'p@example.com', '123', 5, 'Blue', 1);
+
+
+-- Doctors
+INSERT INTO Doctors (id_, fname_,email_, lname_, gender_, experience_, user_id_, qualification_, password_,address_, contact_, location_id_, specialities_id_, description_, status_) VALUES
+(1, 'John','john@gmail.com', 'Doe', 'Male', 10, 1, 'MD', 'doctor_password', '123 Main St', '123-456-7890', 1, 1, 'Experienced cardiologist', 1);
+
+-- Schedules
+INSERT INTO Schedules (id_, doctor_id_, start_time_, end_time_,date_ ) VALUES
+(1, 1, '09:00:00', '12:00:00','2023-08-20');
+
+-- Transactions
+INSERT INTO Transactions (id_, doctor_id_, subscription_id_, payment_date_, amount_, payment_method_) VALUES
+(1, 1, 1, '2023-08-01', 50, 'Credit Card');
+
+-- Patients
+INSERT INTO Patients (id_, user_id_, fname_, lname_, gender_, email_, password_,address_, dob_, contact_) VALUES
+(1, 2, 'Alice', 'Smith', 'Female', 'alice@example.com', 'patient_password','456 Elm St', '1990-05-15', "98765432");
+
+-- Appointments
+INSERT INTO Appointments (id_, patient_id_, time_,doctor_id_, date_, status_) VALUES
+(1, 1, '09:00:00',1,'2023-08-30', 1);
+
+-- DayAvailables
+INSERT INTO DayAvailables (doctor_id_, day_id_) VALUES
+(1, 1),
+(1, 3);
---
+++ Database/script.sql
@@ -0,0 +1,138 @@
+drop database if exists doco;
+CREATE DATABASE IF NOT EXISTS DOCO;
+use DOCO;
+-- Role Table
+CREATE TABLE IF NOT EXISTS Roles (
+    id_ INT PRIMARY KEY NOT NULL,
+role_type_ CHAR(3) NOT NULL
+);
+
+-- Specializations
+CREATE TABLE IF NOT EXISTS Specialities (
+	id_ INT PRIMARY KEY NOT NULL,
+specialities_Name_ VARCHAR(255) NOT NULL
+);
+
+-- Location
+CREATE TABLE IF NOT EXISTS Locations (
+    id_ INT PRIMARY KEY NOT NULL ,
+location_Name_ VARCHAR(255) NOT NULL
+);
+
+-- SubscriptionPlans
+CREATE TABLE IF NOT EXISTS SubscriptionPlans (
+    id_ INT NOT NULL PRIMARY KEY  AUTO_INCREMENT,
+    plan_name_ VARCHAR(255) NOT NULL,
+    descripOfSub_ VARCHAR(255) NOT NULL,
+    monthly_price_ INT NOT NULL,
+    status_ INT NOT NULL NOT NULL
+);
+-- Questions
+CREATE TABLE IF NOT EXISTS  Questions (
+    id_ INT NOT NULL PRIMARY KEY,
+    questions_ VARCHAR(255) NOT NULL
+);
+-- Days
+CREATE TABLE IF NOT EXISTS Days (
+    id_ INT NOT NULL PRIMARY KEY ,
+    day_ VARCHAR(255) NOT NULL
+);
+
+
+-- User table
+CREATE TABLE IF NOT EXISTS Users (
+    id_ INT PRIMARY KEY  AUTO_INCREMENT,
+    role_id_ INT,
+    email_ VARCHAR(255) NOT NULL,
+    password_ VARCHAR(255),
+ question_id_ INT NOT NULL,
+      answer_ VARCHAR(255) NOT NULL,
+    status_ INT NOT NULL,
+    FOREIGN KEY (role_id_) REFERENCES Roles(id_),
+      FOREIGN KEY (question_id_) REFERENCES Questions(id_)
+);
+
+
+-- Doctor
+CREATE TABLE IF NOT EXISTS Doctors (
+    id_ INT PRIMARY KEY NOT NULL  AUTO_INCREMENT,
+	fname_  VARCHAR(255) NOT NULL,
+    email_  VARCHAR(255) NOT NULL,
+    lname_  VARCHAR(255) NOT NULL,
+     gender_  VARCHAR(255) NOT NULL,
+experience_  INT NOT NULL,
+    user_id_ INT NOT NULL,
+    qualification_ VARCHAR(255) NOT NULL,
+    password_ VARCHAR(255) NOT NULL,
+   address_ VARCHAR(255) NOT NULL,
+    contact_ VARCHAR(255) NOT NULL,
+    location_id_ INT NOT NULL,
+    specialities_id_ INT NOT NULL,
+    description_ VARCHAR(255) NOT NULL,
+  
+    status_ INT NOT NULL,
+    FOREIGN KEY (location_id_) REFERENCES Locations (id_),
+    FOREIGN KEY (specialities_id_) REFERENCES Specialities (id_),
+  FOREIGN KEY (user_id_) REFERENCES Users (id_)
+);
+
+
+
+-- Schedule
+CREATE TABLE IF NOT EXISTS Schedules (
+    id_ INT PRIMARY KEY NOT NULL  AUTO_INCREMENT,
+    doctor_id_ INT NOT NULL,
+    start_time_ TIME NOT NULL,
+    end_time_ TIME NOT NULL,
+    date_ Date NOT NULL,
+    FOREIGN KEY (doctor_id_) REFERENCES Doctors(id_)
+);
+
+-- Transactions
+CREATE TABLE IF NOT EXISTS Transactions (
+    id_ INT NOT NULL PRIMARY KEY  AUTO_INCREMENT,
+    doctor_id_ INT NOT NULL,
+    subscription_id_ INT NOT NULL,
+    payment_date_ DATE NOT NULL,
+    amount_ INT NOT NULL,
+    payment_method_ VARCHAR(255) NOT NULL,
+    FOREIGN KEY (doctor_id_) REFERENCES Doctors (id_),
+ FOREIGN KEY (subscription_id_) REFERENCES SubscriptionPlans (id_)
+);
+
+-- Patient Table
+CREATE TABLE IF NOT EXISTS Patients (
+    id_ INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user_id_ int NOT NULL,
+    fname_  VARCHAR(255) NOT NULL,
+    lname_  VARCHAR(255) NOT NULL,
+     gender_  VARCHAR(255) NOT NULL,
+email_ VARCHAR(255) NOT NULL,
+    password_ VARCHAR(255) NOT NULL,
+    address_ VARCHAR(255) NOT NULL,
+dob_ DATE NOT NULL,
+    contact_ Varchar(255) NOT NULL,
+FOREIGN KEY (user_id_) REFERENCES Users (id_)
+
+);
+
+-- Appointments
+CREATE TABLE IF NOT EXISTS Appointments (
+    id_ INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    patient_id_ INT NOT NULL,
+    
+    time_ TIME NOT NULL,
+    doctor_id_ int NOT null,
+    status_ INT NOT NULL,
+    date_ Date NOT NULL,
+    FOREIGN KEY (patient_id_) REFERENCES Patients(id_),
+	FOREIGN KEY (doctor_id_) REFERENCES Doctors(id_)
+);
+
+-- DaysAvailbales
+CREATE TABLE IF NOT EXISTS DayAvailables (
+    doctor_id_ INT NOT NULL,
+    day_id_ INT NOT NULL,
+FOREIGN KEY (doctor_id_) REFERENCES appointments(id_),
+    FOREIGN KEY (day_id_) REFERENCES Days(id_)
+);
